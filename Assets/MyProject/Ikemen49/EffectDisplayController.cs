8400acac018b344b7379b620557392375a3cd44e
a <>
2019-12-14 14:28:02 +0900

    no message

---
+++ Assets/MyProject/Ikemen49/EffectDisplayController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDisplayController : MonoBehaviour {
+
+    public float showTime;
+    public float hideTime;
+    public float currentTime = 0;
+    public bool isShowed = false;
+    public bool isHided = false;
+
+    public GameObject TargetEffect;
+
+	// Use this for initialization
+	void Start () {
+        TargetEffect.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        if(showTime < currentTime && !isShowed)
+        {
+            TargetEffect.SetActive(true);
+            isShowed = true;
+        }else if(currentTime > hideTime && !isHided)
+        {
+            TargetEffect.SetActive(false);
+            isHided = true;
+        }
+    }
+}
